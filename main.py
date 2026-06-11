4e67b08ff2a83ea103e31f503dfb5db226e639a4
Aadil Kakkidi <113379546+AadilKak@users.noreply.github.com>
2026-06-11 19:19:20 -0400

    Front End Connection

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ from sqlalchemy.orm import Session
 from database import init_db, SessionLocal, VehicleListing
 
 app = FastAPI()
-# Add this block
-# Make sure this matches your Vite dev server port
+
+# Automatically build/verify DB tables on script launch
+init_db()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"], 
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
